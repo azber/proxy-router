44bb41ffd51e6746cd012bf07b0bbdc320f8ca5e
azber <azberx@gmail.com>
2017-12-22 15:30:31 +0800

    next ip

--- server/service.go
+++ server/service.go
@@ -6,39 +6,72 @@ import (
 	"io/ioutil"
 	"strings"
 	"errors"
+	"sync"
+	"regexp"
 )
 
 type Service struct {
 	addrList []string
+	mu       *sync.Mutex
 }
 
-func NewService() *Service {
-	return &Service{
+func NewService() (svr *Service) {
+	nextIP := "127.0.0.1:12458"
+
+	svr = &Service{
 		addrList: make([]string, 0),
+		mu:       &sync.Mutex{},
 	}
+	svr.addrList = append(svr.addrList, nextIP)
+	return
 }
 
-func (svr *Service) NextIP() (addr string, err error) {
+func (svr *Service) NextIP(curAddr string) {
+	svr.mu.Lock()
+	defer svr.mu.Unlock()
+
 	if len(svr.addrList) != 0 {
-		addr = svr.addrList[0]
+		if curAddr != svr.addrList[0] {
+			return
+		}
 		svr.addrList = svr.addrList[1:]
+	}
+
+	list, err := svr.requestGetIP(svr.GetDefaultParams())
+	if err != nil {
 		return
 	}
-	list, err := svr.GetIP(svr.GetDefaultParams())
+	for _, it := range list {
+		r, _ := regexp.Compile(`((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))`)
+		if r.Match([]byte(it)) {
+			svr.addrList = append(svr.addrList, it)
+		}
+	}
+	return
+}
+
+func (svr *Service) GetIP() (addr string, err error) {
+	svr.mu.Lock()
+	defer svr.mu.Unlock()
+
+	if len(svr.addrList) != 0 {
+		return svr.addrList[0], nil
+	}
+	list, err := svr.requestGetIP(svr.GetDefaultParams())
 	if err != nil {
 		return
 	}
 	if len(list) > 0 {
 		addr = list[0]
-		svr.addrList = list[1:]
+		svr.addrList = append(svr.addrList, list...)
+		return
 	} else {
 		err = errors.New("err, zhima get ip fatal")
 		return
 	}
-	return
 }
 
-func (svr *Service) GetIP(num, dataType, pro, city, yys, port, pack, ts, ys, cs, lb, sb, pb, mr int) (addrList []string, err error) {
+func (svr *Service) requestGetIP(num, dataType, pro, city, yys, port, pack, ts, ys, cs, lb, sb, pb, mr int) (addrList []string, err error) {
 	reqUrl := fmt.Sprintf("http://webapi.http.zhimacangku.com/getip?num=%d&type=%d&pro=%d&city=%d&yys=%d&port=%d&pack=%d&ts=%d&ys=%d&cs=%d&lb=%d&sb=%d&pb=%d&mr=%d&regions=",
 		num, dataType, pro, city, yys, port, pack, ts, ys, cs, lb, sb, pb, mr)
 	res, err := http.Get(reqUrl)
@@ -51,7 +84,7 @@ func (svr *Service) GetIP(num, dataType, pro, city, yys, port, pack, ts, ys, cs,
 		return
 	}
 	resStr := string(resBody)
-	addrList = strings.Split(resStr, "\n")
+	addrList = strings.Split(resStr, "\r\n")
 	return
 }
 
